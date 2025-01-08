a5d440163bae719e2bb2ca7f8491d435475c3c26
fatihkayas <seavsdeep@gmail.com>
2025-01-08 16:53:20 +0100

    Add injection

--- getting-started/src/main/java/org/acme/GreetingResource.java
+++ getting-started/src/main/java/org/acme/GreetingResource.java
@@ -1,12 +1,24 @@
 package org.acme;
 
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
 @Path("/hello")
 public class GreetingResource {
+
+    @Inject
+    GreetingService service;
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/greeting/{name}")
+    public String greeting(@PathParam("name") String name) {
+        return service.greeting(name);
+    }
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
